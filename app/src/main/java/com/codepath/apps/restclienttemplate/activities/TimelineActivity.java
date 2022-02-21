0a81200b7d6d20affb3cbe6726c9946eeb6a3b78
Mohammed Rahman <minhaz357@gmail.com>
2022-02-21 13:23:25 -0500

    Send GET request to the twitter home_timeline api
    	-GET https://api.twitter.com/1.1/statuses/home_timeline.json

--- app/src/main/java/com/codepath/apps/restclienttemplate/activities/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/activities/TimelineActivity.java
@@ -3,17 +3,44 @@ package com.codepath.apps.restclienttemplate.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.codepath.apps.restclienttemplate.R;
+import com.codepath.apps.restclienttemplate.application.TwitterApp;
+import com.codepath.apps.restclienttemplate.client.TwitterClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
 import java.util.Objects;
 
+import okhttp3.Headers;
+
 public class TimelineActivity extends AppCompatActivity {
+
+    public static String TAG = "TimelineActivity";
+
+    TwitterClient twitterClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
         Objects.requireNonNull(getSupportActionBar()).setTitle("Tweets");
+
+        twitterClient = TwitterApp.getRestClient(this);
+        populateHomeTimeline();
+    }
+
+    private void populateHomeTimeline() {
+        twitterClient.getHomeTimeline(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.i(TAG, "onSuccess." + json.toString());
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.i(TAG, "onFailure." + response,throwable);
+            }
+        });
     }
 }
