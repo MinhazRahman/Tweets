8b234dde496cdb011e93670918ca0003a70b2208
Mohammed Rahman <minhaz357@gmail.com>
2022-02-22 14:48:51 -0500

    Add decorators to each recyclerview item

--- app/src/main/java/com/codepath/apps/restclienttemplate/activities/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/activities/TimelineActivity.java
@@ -1,6 +1,7 @@
 package com.codepath.apps.restclienttemplate.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -12,6 +13,7 @@ import com.codepath.apps.restclienttemplate.adapters.TweetsAdapter;
 import com.codepath.apps.restclienttemplate.application.TwitterApp;
 import com.codepath.apps.restclienttemplate.client.TwitterClient;
 import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.apps.restclienttemplate.utils.SpacesItemDecoration;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
@@ -31,6 +33,8 @@ public class TimelineActivity extends AppCompatActivity {
     RecyclerView rvTweets;
     List<Tweet> tweets;
     TweetsAdapter tweetsAdapter;
+    RecyclerView.ItemDecoration itemDecoration;
+    SpacesItemDecoration spacesItemDecoration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +54,16 @@ public class TimelineActivity extends AppCompatActivity {
         // Setup the RecyclerView: layout manager and the adapter
         rvTweets.setLayoutManager(new LinearLayoutManager(this));
         rvTweets.setAdapter(tweetsAdapter);
+
+        // decorate each item using decorators attached to the recyclerview
+        itemDecoration = new
+                DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+
+        // attach a decorator to  the recyclerview for adding consistent spacing around items displayed
+        spacesItemDecoration = new SpacesItemDecoration(16);
+
+        rvTweets.addItemDecoration(itemDecoration);
+        rvTweets.addItemDecoration(spacesItemDecoration);
 
         populateHomeTimeline();
     }
