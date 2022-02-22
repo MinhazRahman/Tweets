4c0b490f4ab8c6c8b7a4d50a0cb7069bbbca8343
Mohammed Rahman <minhaz357@gmail.com>
2022-02-22 14:34:07 -0500

    Setup the layout manager and the adapter for RecyclerView

--- app/src/main/java/com/codepath/apps/restclienttemplate/activities/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/activities/TimelineActivity.java
@@ -1,15 +1,24 @@
 package com.codepath.apps.restclienttemplate.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 
 import com.codepath.apps.restclienttemplate.R;
+import com.codepath.apps.restclienttemplate.adapters.TweetsAdapter;
 import com.codepath.apps.restclienttemplate.application.TwitterApp;
 import com.codepath.apps.restclienttemplate.client.TwitterClient;
+import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import okhttp3.Headers;
@@ -19,6 +28,9 @@ public class TimelineActivity extends AppCompatActivity {
     public static String TAG = "TimelineActivity";
 
     TwitterClient twitterClient;
+    RecyclerView rvTweets;
+    List<Tweet> tweets;
+    TweetsAdapter tweetsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +39,18 @@ public class TimelineActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).setTitle("Tweets");
 
         twitterClient = TwitterApp.getRestClient(this);
+
+        // Find the recycler view
+        rvTweets = findViewById(R.id.rvTweets);
+
+        // Initialize the list of tweets and adapter
+        tweets = new ArrayList<>();
+        tweetsAdapter = new TweetsAdapter(this, tweets);
+
+        // Setup the RecyclerView: layout manager and the adapter
+        rvTweets.setLayoutManager(new LinearLayoutManager(this));
+        rvTweets.setAdapter(tweetsAdapter);
+
         populateHomeTimeline();
     }
 
@@ -35,6 +59,13 @@ public class TimelineActivity extends AppCompatActivity {
             @Override
             public void onSuccess(int statusCode, Headers headers, JSON json) {
                 Log.i(TAG, "onSuccess." + json.toString());
+                JSONArray jsonArray = json.jsonArray;
+                try {
+                    tweets.addAll(Tweet.fromJsonArray(jsonArray));
+                    tweetsAdapter.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    Log.i(TAG, "Json exception", e);
+                }
             }
 
             @Override
